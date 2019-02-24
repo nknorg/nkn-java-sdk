9b5b5a489bc76d9eaf8cdbbbbc6b28de50abc78c
John Smith <jsmith@jsmith.cz>
2019-02-24 14:58:00 -0600

    Add more error code constants

--- src/main/java/jsmith/nknsdk/client/ErrorCodes.java
+++ src/main/java/jsmith/nknsdk/client/ErrorCodes.java
@@ -7,9 +7,24 @@ public class ErrorCodes {
 
     public static final int SUCCESS = 0;
 
-    public static final int WRONG_NODE = 48001;
-
-    // TODO get all other codes
+    public static final int SESSION_EXPIRED     = 41001;
+    public static final int SERVICE_CEILING     = 41002;
+    public static final int ILLEGAL_DATAFORMAT  = 41003;
+    public static final int INVALID_METHOD      = 42001;
+    public static final int INVALID_PARAMS      = 42002;
+    public static final int INVALID_TOKEN       = 42003;
+    public static final int INVALID_TRANSACTION = 43001;
+    public static final int INVALID_ASSET       = 43002;
+    public static final int INVALID_BLOCK       = 43003;
+    public static final int INVALID_HASH        = 43004;
+    public static final int INVALID_VERSION     = 43005;
+    public static final int UNKNOWN_TRANSACTION = 44001;
+    public static final int UNKNOWN_ASSET       = 44002;
+    public static final int UNKNOWN_BLOCK       = 44003;
+    public static final int UNKNOWN_HASH        = 44004;
+    public static final int INTERNAL_ERROR      = 45001;
+    public static final int SMARTCODE_ERROR     = 47001;
+    public static final int WRONG_NODE          = 48001;
 
 
 }
