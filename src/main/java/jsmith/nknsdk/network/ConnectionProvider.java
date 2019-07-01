a00dc92b808dba6e149deff445fc23aae25f99d3
John Smith <jsmith@jsmith.cz>
2019-07-01 14:21:56 +0200

    Update connection provider to mainnet

--- src/main/java/jsmith/nknsdk/network/ConnectionProvider.java
+++ src/main/java/jsmith/nknsdk/network/ConnectionProvider.java
@@ -17,32 +17,51 @@ public class ConnectionProvider {
 
     private static InetSocketAddress[] bootstrapNodes = {
 
-            // Legacy
-//            new InetSocketAddress("cluster2-oregon.nkn.org", 30003),
-//            new InetSocketAddress("node00002.nkn.org", 30003),
-//            new InetSocketAddress("104.196.227.157", 30003),
+            new InetSocketAddress("mainnet-seed-0001.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0002.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0003.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0004.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0005.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0006.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0007.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0008.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0009.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0010.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0011.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0012.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0013.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0014.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0015.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0016.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0017.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0018.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0019.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0020.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0021.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0022.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0023.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0024.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0025.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0026.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0027.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0028.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0029.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0030.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0031.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0032.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0033.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0034.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0035.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0036.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0037.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0038.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0039.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0040.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0041.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0042.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0043.nkn.org", 30003),
+            new InetSocketAddress("mainnet-seed-0044.nkn.org", 30003)
 
-
-            // Local
-//            new InetSocketAddress("localhost", 30003),
-
-
-            // Testnet
-//            new InetSocketAddress("testnet-seed-0001.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0002.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0003.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0004.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0005.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0006.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0007.nkn.org", 30003),
-//            new InetSocketAddress("testnet-seed-0008.nkn.org", 30003)
-
-
-            // Devnet
-            new InetSocketAddress("35.205.185.106", 30003),
-            new InetSocketAddress("104.199.98.226", 30003),
-            new InetSocketAddress("35.204.159.254", 30003),
-            new InetSocketAddress("35.224.16.68", 30003)
     };
 
     private static int maxRetries = 3;
