ec25f64959f837f2fb949f2eac3b748d8b1310e3
John Smith <jsmith@jsmith.cz>
2019-07-18 22:07:11 +0200

    Resolve bootstrap nodes IPs lazily

--- src/main/java/jsmith/nknsdk/network/ConnectionProvider.java
+++ src/main/java/jsmith/nknsdk/network/ConnectionProvider.java
@@ -4,6 +4,7 @@ import jsmith.nknsdk.utils.ThrowingLambda;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 /**
@@ -15,52 +16,52 @@ public class ConnectionProvider {
 
     private static final Object lock = new Object();
 
-    private static InetSocketAddress[] bootstrapNodes = {
+    private static String[] bootstrapNodes = {
 
-            new InetSocketAddress("mainnet-seed-0001.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0002.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0003.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0004.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0005.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0006.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0007.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0008.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0009.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0010.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0011.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0012.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0013.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0014.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0015.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0016.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0017.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0018.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0019.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0020.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0021.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0022.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0023.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0024.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0025.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0026.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0027.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0028.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0029.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0030.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0031.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0032.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0033.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0034.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0035.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0036.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0037.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0038.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0039.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0040.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0041.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0042.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0043.nkn.org", 30003),
-            new InetSocketAddress("mainnet-seed-0044.nkn.org", 30003)
+            "mainnet-seed-0001.nkn.org:30003",
+            "mainnet-seed-0002.nkn.org:30003",
+            "mainnet-seed-0003.nkn.org:30003",
+            "mainnet-seed-0004.nkn.org:30003",
+            "mainnet-seed-0005.nkn.org:30003",
+            "mainnet-seed-0006.nkn.org:30003",
+            "mainnet-seed-0007.nkn.org:30003",
+            "mainnet-seed-0008.nkn.org:30003",
+            "mainnet-seed-0009.nkn.org:30003",
+            "mainnet-seed-0010.nkn.org:30003",
+            "mainnet-seed-0011.nkn.org:30003",
+            "mainnet-seed-0012.nkn.org:30003",
+            "mainnet-seed-0013.nkn.org:30003",
+            "mainnet-seed-0014.nkn.org:30003",
+            "mainnet-seed-0015.nkn.org:30003",
+            "mainnet-seed-0016.nkn.org:30003",
+            "mainnet-seed-0017.nkn.org:30003",
+            "mainnet-seed-0018.nkn.org:30003",
+            "mainnet-seed-0019.nkn.org:30003",
+            "mainnet-seed-0020.nkn.org:30003",
+            "mainnet-seed-0021.nkn.org:30003",
+            "mainnet-seed-0022.nkn.org:30003",
+            "mainnet-seed-0023.nkn.org:30003",
+            "mainnet-seed-0024.nkn.org:30003",
+            "mainnet-seed-0025.nkn.org:30003",
+            "mainnet-seed-0026.nkn.org:30003",
+            "mainnet-seed-0027.nkn.org:30003",
+            "mainnet-seed-0028.nkn.org:30003",
+            "mainnet-seed-0029.nkn.org:30003",
+            "mainnet-seed-0030.nkn.org:30003",
+            "mainnet-seed-0031.nkn.org:30003",
+            "mainnet-seed-0032.nkn.org:30003",
+            "mainnet-seed-0033.nkn.org:30003",
+            "mainnet-seed-0034.nkn.org:30003",
+            "mainnet-seed-0035.nkn.org:30003",
+            "mainnet-seed-0036.nkn.org:30003",
+            "mainnet-seed-0037.nkn.org:30003",
+            "mainnet-seed-0038.nkn.org:30003",
+            "mainnet-seed-0039.nkn.org:30003",
+            "mainnet-seed-0040.nkn.org:30003",
+            "mainnet-seed-0041.nkn.org:30003",
+            "mainnet-seed-0042.nkn.org:30003",
+            "mainnet-seed-0043.nkn.org:30003",
+            "mainnet-seed-0044.nkn.org:30003"
 
     };
 
@@ -105,7 +106,7 @@ public class ConnectionProvider {
         }
     }
 
-    public static void setBootstrapNodes(InetSocketAddress[] nodes) {
+    public static void setBootstrapNodes(String[] nodes) {
         synchronized (lock) {
             bootstrapNodes = nodes;
         }
@@ -115,7 +116,7 @@ public class ConnectionProvider {
         final int retries = maxRetries();
         Exception error = null;
 
-        final InetSocketAddress[] nodes;
+        final String[] nodes;
         synchronized (lock) {
             nodes = bootstrapNodes;
         }
@@ -124,7 +125,20 @@ public class ConnectionProvider {
 
         for (int i = 0; i <= retries; i++) {
             try {
-                return action.apply(nodes[nextNodeI]);
+                String hostname; int port;
+                try {
+                    final String addr = nodes[nextNodeI];
+                    final int idx = addr.lastIndexOf(':');
+                    hostname = addr.substring(0, idx);
+                    port = Integer.parseInt(addr.substring(idx + 1));
+                } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                    LOG.warn("Attempt {} failed", i);
+                    LOG.warn("Invalid host address");
+                    nextNodeI ++;
+                    if (nextNodeI >= nodes.length) nextNodeI -= nodes.length;
+                    continue;
+                }
+                return action.apply(new InetSocketAddress(hostname, port));
             } catch (Exception t) {
                 error = t;
                 LOG.warn("Attempt {} failed", i);
